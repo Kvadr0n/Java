50d45150f7653ebcaaa526b825cbb4036460157c
Kvadr0n <anlimirea@yandex.ru>
2021-09-16 11:36:50 +0300

    Task2InProgress

--- src/tasks/task2/TestShape.java
+++ src/tasks/task2/TestShape.java
@@ -5,6 +5,6 @@ public class TestShape
     public static void main(String[] args)
     {
         Shape Bonk = new Shape("Big", "Orange", "Ball");
-        System.out.println("Bonk is " + Bonk);
+        System.out.println("Bonk is " + Bonk + ".");
     }
 }
